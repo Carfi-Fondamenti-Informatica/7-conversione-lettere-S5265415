cd4e54644a1ab1cb287fe75c445031825909735a
S5265415 <101595525+S5265415@users.noreply.github.com>
2022-05-07 15:56:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ int main(){
   int n=(int)a;
   bool b=caratteri(n);
   if(b==1){
-    if ((65<=n) && (n>=90)){
+    if ((65<=n) && (n<=90)){
       n=n+32;
-      cout<< (char)n<< endl;
+      cout<< (char)n << endl;
     }
    else if((97<=n) && (n<=122)){
      n=n-32;
